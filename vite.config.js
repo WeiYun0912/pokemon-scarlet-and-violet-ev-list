43f71d31750409079c08e0ed4573ca58cdd09c8c
WeiWei <yher25@gmail.com>
2023-10-13 13:15:44 +0800

    test worker

--- vite.config.js
+++ vite.config.js
@@ -4,33 +4,53 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 const manifestForPlugin = {
-  registerType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  registerType: "autoUpdate",
+  includeAssets: ["**/*"],
+  devOptions: {
+    enabled: true,
+  },
+  workbox: {
+    globPatterns: ["**/*.{js,jsx,css,html,pdf,json}"],
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) => {
+          return url.pathname.startsWith("/");
+        },
+        handler: "CacheFirst",
+        options: {
+          cacheName: "cache",
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
   manifest: {
     name: "Pokemon EV",
     short_name: "Pokemon EV",
     description: "Pokemon EV",
     icons: [
       {
-        src: "eevee-512x512.png",
+        src: "/pokemon-scarlet-and-violet-ev-list/eevee-512x512.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "monochrome",
       },
       {
-        src: "eevee-512x512.png",
+        src: "/pokemon-scarlet-and-violet-ev-list/eevee-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "eevee-512x512.png",
+        src: "/pokemon-scarlet-and-violet-ev-list/eevee-512x512.png",
         sizes: "180x180",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "eevee-512x512.png",
+        src: "/pokemon-scarlet-and-violet-ev-list/eevee-512x512.png",
         sizes: "225x225",
         type: "image/png",
         purpose: "maskable",
@@ -39,8 +59,8 @@ const manifestForPlugin = {
     theme_color: "#171717",
     background_color: "#e8ebf2",
     display: "standalone",
-    scope: "/",
-    start_url: "/",
+    scope: "/pokemon-scarlet-and-violet-ev-list/",
+    start_url: "/pokemon-scarlet-and-violet-ev-list/",
     orientation: "portrait",
   },
 };
