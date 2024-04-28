968e4f93d526d6f3e62ef288ce54761a535b95cf
WeiWei <yher25@gmail.com>
2024-04-29 01:44:52 +0800

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pokemon-scarlet-and-violet-ev-list/sw.js', { scope: '/pokemon-scarlet-and-violet-ev-list/' })})}
